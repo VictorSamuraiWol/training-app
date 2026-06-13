127d37b5918fd5ac415feed1af331f09e4a11e5a
Victor Cardoso <victorclmatos@gmail.com>
2026-06-13 10:28:49 -0300

    remove some comments in Footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,105 +4,16 @@ import Loader from '../Loader/Loader';
 import QuickAccessFooter from '../QuickAccessFooter/QuickAccessFooter';
 import { DataContext } from '../DataContext/DataContext';
 import { TbTriangleInvertedFilled } from "react-icons/tb";
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 
 function Footer() {
 
-  const { loader, 
-    
-
-    
-    // staticUsersContents, dbUsers, loginValidate, nameUser, typeTrain, setTypeTrain, 
-    // ableExercisesPage, ableNutritionPage, ableNotesPage 
-  } = useContext(DataContext)
-
-  // const navigate = useNavigate()
-
-  // const [openQuickAccess, setOpenQuickAccess] = useState(false)
-
-  // const [disabledSelectNamesQuickAccess, setDisabledSelectNamesQuickAccess] = useState(false)
-  
-  // const handleNavigate = (e) => {
-  //   const path = e.target.value
-  //   let letterTypeTrain
-
-  //   if (path.includes('/exercises-page/')) {
-  //     letterTypeTrain = path.split('/').pop()
-      
-  //   }
-
-  //   setTypeTrain(letterTypeTrain)
-
-  //   if (path) navigate(path)
-
-  //   return letterTypeTrain
-
-  // }
-
-  // function optionSelect(optionSelect) {
-  //   let select
-
-  //   if (optionSelect.includes(typeTrain)) {
-  //     select = 'true'
-      
-  //   }    
-
-  //   return select
-
-  // }
+  const { loader } = useContext(DataContext)
 
   return(
     <div className='footer'>
-
-
-
       <QuickAccessFooter />
-
-
-
-      {/* {(ableExercisesPage || ableNutritionPage || ableNotesPage) && 
-      <div className='quick-access-selects'>        
-        <TbTriangleInvertedFilled className={`quick-access-selects-icon ${openQuickAccess ? 
-          "quick-access-selects-icon-animation" : ""}`} 
-        />
-
-        <select
-          onClick={() => setDisabledSelectNamesQuickAccess(true)} 
-          onChange={(e) => {handleNavigate(e); setOpenQuickAccess(false)}}
-          onFocus={() => setOpenQuickAccess(true)}
-          onBlur={() => {setOpenQuickAccess(false); setDisabledSelectNamesQuickAccess(false)}}
-          onMouseDown={() => setOpenQuickAccess(true)}
-          className='quick-access-selects-select'
-        >
-          <option
-            value=''
-            disabled={disabledSelectNamesQuickAccess}
-            className='quick-access-selects-select-option'       
-          >
-            Quick Access
-          </option>
-
-          {(staticUsersContents || dbUsers) && loginValidate && [...(staticUsersContents), ...(dbUsers)]
-          .filter(user => user.name.toLowerCase() === nameUser.toLowerCase().trim())
-          .map(user => user.quick_access && user.quick_access)
-          .map(quickAccessList => quickAccessList)[0]
-          .map(quickAccess => (
-            <option 
-              key={quickAccess[0].name}
-              value={quickAccess[1].path}
-              className={`'quick-access-selects-select-option' 
-                ${optionSelect(quickAccess[1].path) === 'true' && 'quick-access-selects-select-option-select'}`}
-              onClick={() => setDisabledSelectNamesQuickAccess(true)}
-              path={optionSelect(quickAccess[1].path)}
-            >
-              {quickAccess[0].name}
-            </option>
-          ))}
-
-        </select>
-
-      </div>} */}
 
       <img 
         className='footer-img' 
